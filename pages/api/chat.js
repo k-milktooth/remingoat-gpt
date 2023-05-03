9332663f9eae120cefab39505f24840d6db3c520
k-milktooth <k-milktooth@users.noreply.github.com>
2023-05-02 18:27:02 -0700

    feat: Create chat API endpoint
    - Add new `chat.js` API endpoint
    - Validate and sanitize POST request for `question`
    - Use OpenAI embeddings to create `vectorstore` and `chain`
    - Send response to client with JSON data `response`

---
+++ pages/api/chat.js
@@ -0,0 +1,49 @@
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { makeChain } from "@/utils/makechain";
+import { pinecone } from "@/utils/pinecone-client";
+
+export default async function handler(req, res) {
+  const { question, history } = req.body;
+
+  // Only accept post requests
+  if (req.method !== "POST") {
+    res.status(405).json({ error: "Method not allowed" });
+    return;
+  }
+
+  // Check if question is empty
+  if (!question) {
+    res.status(400).json({ error: "Question is empty" });
+    return;
+  }
+
+  // OpenAI recommends replacing newlines with spaces for best results
+  const sanitizedQuestion = question.trim().replaceAll("\n", " ");
+
+  try {
+    const pineconeIndex = pinecone.Index(`${process.env.PINECONE_INDEX}`);
+
+    // Create vectorstore
+    const vectorStore = await PineconeStore.fromExistingIndex(
+      new OpenAIEmbeddings(),
+      {
+        pineconeIndex,
+      }
+    );
+
+    // Create chain
+    const chain = makeChain(vectorStore);
+
+    // Ask a question using chat history
+    const response = await chain.call({
+      question: sanitizedQuestion,
+      chat_history: history || [],
+    });
+
+    res.status(200).json({ response });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message || "Something went wrong" });
+  }
+}
