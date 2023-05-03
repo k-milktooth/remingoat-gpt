8dcb1cb645fd0d635a3e0a0e9cc27b551c4041f6
k-milktooth <k-milktooth@users.noreply.github.com>
2023-05-03 15:32:09 -0700

    feat: Add constants for object identifiers in utils.
    - Create new file `utils/object-identifiers.js`
    - Export constants `end`, `message` and `sourceDocuments` in `utils/object-identifiers.js`

---
+++ utils/object-identifiers.js
@@ -0,0 +1,7 @@
+/**
+ * Strings representing the object's type in server-sent events
+ * Used in identifying streaming events from the server to the front-end
+ */
+export const end = "end";
+export const message = "message";
+export const sourceDocuments = "sourceDocuments";
