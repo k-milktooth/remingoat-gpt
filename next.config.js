b4bed13793fcc63d4f7b11b61041b8963092cc1b
k-milktooth <k-milktooth@users.noreply.github.com>
2023-05-06 13:36:05 -0700

    feat: Improve image domain support in Next.js config
    - Add support for specific image domains in `next.config.js`

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["user-images.githubusercontent.com"],
+  },
   webpack(config) {
     config.experiments = { ...config.experiments, topLevelAwait: true };
     return config;
