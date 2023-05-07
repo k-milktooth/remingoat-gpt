27c3049aecbeda72a3ad0b49e590c2667903eb5a
k-milktooth <k-milktooth@users.noreply.github.com>
2023-05-06 18:17:05 -0700

    feat: Add PostHog analytics to site.
    Add PostHog provider and client-side initialization for page view tracking with PostHog

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,42 @@
 import "@/styles/globals.css";
 import Layout from "@/components/layouts/layout";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+// Check that PostHog is client-side
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
+    session_recording: {
+      maskAllInputs: false,
+    },
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug();
+    },
+  });
+}
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Track page views
+    const handleRouteChange = () => posthog?.capture("$pageview");
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <PostHogProvider client={posthog}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </PostHogProvider>
   );
 }
